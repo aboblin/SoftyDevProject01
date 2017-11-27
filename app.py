9904d511fbe1609ab89835986f440ba6bb58cead
Karen Chen <kchen6600@gmail.com>
2017-11-27 14:01:31 -0500

    creating input form

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def root():
-	r = requests.get("https://api.nasa.gov/planetary/apod?api_key=CJIKeQKz4nuOpRSiMmW2qWB7qylNrE717O2q30Va")
-	dictionary = r.json()
-	return render_template('index.html', pic = dictionary['url'], info = dictionary["explanation"])
+	#r = requests.get("https://api.nasa.gov/planetary/apod?api_key=CJIKeQKz4nuOpRSiMmW2qWB7qylNrE717O2q30Va")
+	#dictionary = r.json()
+        if request.method == 'POST':
+                return render_template('recipes.html')
+        else:
+	        return render_template('index.html')
+
 	
 if __name__ == '__main__':
 	app.run(debug = True)
