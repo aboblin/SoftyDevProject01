77e7e6cd3cf5a9f99a8adcc26d6dc8fbf8426d7a
aboblin <aboylan@stuy.edu>
2017-11-28 16:08:39 -0500

    edited recipe_func.py

--- utils/recipe_func.py
+++ utils/recipe_func.py
@@ -12,14 +12,14 @@ def api_to_json(api_link):
 	return url.json()
 
 def search_json(ingredients):
-	api_link = "http://food2fork.com/api/search?api=API_KEY&q="
+	api_link = "http://food2fork.com/api/search?key=90c4ebe5b63a775a3d75b20f0dab1906&q="
 	for ingredient in ingredients:
 		api_link += ingredient + ','
 	return api_to_json(api_link)
 
 def recipe_json(recipeID):
-	api_link = "http://food2fork.com/api/get?api=API_KEY&rID=" + str(recipeID)
-	return api_to_json(api_link)
+	api_link = "http://food2fork.com/api/get?key=90c4ebe5b63a775a3d75b20f0dab1906&rId=" + str(recipeID)
+	return api_to_json(api_link)['recipe']
 
 def get_ingredients(recipeID):
 	return recipe_json(recipeID)['ingredients']
@@ -35,8 +35,8 @@ def get_publisher(recipeID):
 
 if __name__ == "__main__":
 	print search_json(['cheese', 'bread'])
-	print recipe_json(12345)
-	print get_ingredients(12345)
-	print get_source_url(12345)
-	print get_title(12345)
-	print get_publisher(12345)
+	print recipe_json(35382)
+	print get_ingredients(35382)
+	print get_source_url(35382)
+	print get_title(35382)
+	print get_publisher(35382)
