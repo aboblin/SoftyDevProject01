067c544541cfa49aa989bc20b12aaaf338a1a3f5
aboblin <aboylan@stuy.edu>
2017-11-28 13:31:28 -0500

    edited recipe_func.py

--- utils/recipe_func.py
+++ utils/recipe_func.py
@@ -1,6 +1,36 @@
-from flask import Flask
 import json, requests
 
-url = requests.get("http://food2fork.com/api/search?key=90c4ebe5b63a775a3d75b20f0dab1906&q=shredded%20chicken")
+#Input an api_link, returns json of data recieved from the api
+def api_to_json(api_link):
+	url = requests.get(api_link)
+	return url.json()
 
-print url.json();
+def search_json(ingredients):
+	api_link = "http://food2fork.com/api/search?api=API_KEY&q="
+	for ingredient in ingredients:
+		 api_link += ingredient + ','
+	return api_to_json(api_link)
+
+def recipe_json(recipeID):
+	api_link = "http://food2fork.com/api/get?api=API_KEY&rID=" + str(recipeID)
+	return api_to_json(api_link)
+
+def get_ingredients(recipeID):
+	return recipe_json(recipeID)['ingredients']
+
+def get_source_url(recipeID):
+	return recipe_json(recipeID)['source_url']
+
+def get_title(recipeID):
+	return recipe_json(recipeID)['title']
+
+def get_publisher(recipeID):
+	return recipe_json(recipeID)['publisher']
+
+if __name__ == "__main__":
+	print search_json(['cheese', 'bread'])
+	print recipe_json(12345)
+	print get_ingredients(12345)
+	print get_source_url(12345)
+	print get_title(12345)
+	print get_publisher(12345)
