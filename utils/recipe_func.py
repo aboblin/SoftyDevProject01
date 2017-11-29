6f6f96bfd84650218d11a182b0aa694df586dac0
aboblin <aboylan@stuy.edu>
2017-11-28 21:59:56 -0500

    edited recipe_func.py

--- utils/recipe_func.py
+++ utils/recipe_func.py
@@ -1,50 +1,39 @@
 import json, requests
 
 def getKey():
-        f = open('key','r')
+        f = open('utils/key','r')
         key = f.read()
         f.close()
         return key
-	
 
-
-#input an api_link, returns json of data recieved from the api
+#input an api_link, returns json recieved from the api
 def api_to_json(api_link):
 	url = requests.get(api_link)
 	return url.json()
 
+#give a list of ingredients, function uses a search request to receive json and returns that json
 def search_json(ingredients):
-	api_link = "http://food2fork.com/api/search?key=90c4ebe5b63a775a3d75b20f0dab1906&q="
+	api_link = "http://food2fork.com/api/search?key=%s&q=" % getKey()
 	for ingredient in ingredients:
 		api_link += ingredient + ','
-	return api_to_json(api_link)
+	return api_to_json(api_link)['recipes']
 
+#give a recipe Id (received from the search_json), function uses a get request to receive json and returns that json
 def recipe_json(recipeID):
-	api_link = "http://food2fork.com/api/get?key=90c4ebe5b63a775a3d75b20f0dab1906&rId=" + str(recipeID)
+	api_link = "http://food2fork.com/api/get?key=%s&rId=" % getKey()
+	api_link += str(recipeID)
 	return api_to_json(api_link)['recipe']
 
+#creates a dictionary of the ingredients from a recipe in the format {ingredient_name:[amount, unit_of_measurement]}
 def get_ingredients_dict(recipeID):
 	dic = {}
 	for ingredient in recipe_json(recipeID)['ingredients']:
 		ing_list = ingredient.split(' ')
 		removable_parts = ing_list[0].encode("utf-8") + " " + ing_list[1].encode("utf-8")
 		dic[ingredient[len(removable_parts)+1:].encode("utf-8")] = [ing_list[0].encode("utf-8"), ing_list[1].encode("utf-8")]
-	print dic
-		
-
-def get_source_url(recipeID):
-	return recipe_json(recipeID)['source_url']
-
-def get_title(recipeID):
-	return recipe_json(recipeID)['title']
-
-def get_publisher(recipeID):
-	return recipe_json(recipeID)['publisher']
+	return dic
 
 if __name__ == "__main__":
-	#print search_json(['cheese', 'bread'])
-	#print recipe_json(35382)
+	print search_json(['cheese', 'bread'])
+	print recipe_json(35382)
 	print get_ingredients_dict(35382)
-	#print get_source_url(35382)
-	#print get_title(35382)
-	#print get_publisher(35382)
