04ca9f67ca34901aada4e42b41eaee4b04797dd7
aboblin <aboylan@stuy.edu>
2017-11-28 22:29:35 -0500

    added helper funcs to recipe_func.py

--- utils/recipe_func.py
+++ utils/recipe_func.py
@@ -18,6 +18,15 @@ def search_json(ingredients):
 		api_link += ingredient + ','
 	return api_to_json(api_link)['recipes']
 
+def recipe_title(json):
+	return json['title']
+
+def recipe_source_url(json):
+	return json['source_url']
+
+def recipe_id(json):
+	return json['recipe_id']
+
 #give a recipe Id (received from the search_json), function uses a get request to receive json and returns that json
 def recipe_json(recipeID):
 	api_link = "http://food2fork.com/api/get?key=%s&rId=" % getKey()
