39a5322bc633d2ea5de79b0e75aa74162b81b0a2
clei1 <clei1@stuy.edu>
2017-11-29 21:14:33 -0500

    working forms/sending correct info

--- v2/app.py
+++ v2/app.py
@@ -8,45 +8,31 @@ app = Flask(__name__)
 
 def findRecipes(item):
        recipes=search_json(item)
-       if request.form["carb"] == "":
-              carbamount = 0
-       else:
-              carbamount = float(request.form["carb"])
-       if request.form["protein"] == "":
-              proteinamount = 0
-       else:
-              proteinamount = float(request.form["protein"])
-       if request.form["fat"] == "":
-              fatamount = 0
-       else:
-              fatamount = float(request.form["fat"])
        recipeIDs = []
        for recipe in recipes:
               recipeIDs.append(recipe_id(recipe))
        differences = []
-       for recipeID in recipeIDs:
-              nutrient_info = sumNutri(addDetails(get_ingredients_dict(recipeID)))
-              differences.append(abs(nutrient_info[0] - carbamount) + abs(nutrient_info[1] - proteinamount) + abs(nutrient_info[2] - fatamount))
-       chosen = []
-       chosen1 = recipeIDs[differences.index(min(differences))]
-       chosen.append(chosen1)
-       differences.pop(differences.index(min(differences)))
-       chosen2 = recipeIDs[differences.index(min(differences))]
-       chosen.append(chosen2)
-       differences.pop(differences.index(min(differences)))
-       chosen3 = recipeIDs[differences.index(min(differences))]
-       chosen.append(chosen3)
-       differences.pop(differences.index(min(differences)))
+
        return chosen
 
 @app.route('/', methods=['GET','POST'])
 def root():
        if request.method == 'POST':
-              search = request.form["food"]
-              return render_template('temp.html', search = search)
+              info = getInfo()
+              return render_template('temp.html', search = info['search'])
        else:
               return render_template('index.html')
 
-	
+def getInfo():
+       info = {'protein': 0.0, 'carb': 0.0, 'fat': 0.0}
+       info['search'] = request.form['food']
+       if request.form["protein"] != "":
+              info['protein'] = float(request.form["protein"])
+       if request.form["carb"] != "":
+              info['carb'] = float(request.form['carb'])
+       if request.form['fat'] != "":
+              info['fat'] = float(request.form['fat'])    
+       return info
+
 if __name__ == '__main__':
 	app.run(debug = True)
