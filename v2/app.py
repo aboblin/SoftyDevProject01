3e475fbbbf9f95fe691d055222196a36d5d53b6a
clei1 <clei1@stuy.edu>
2017-12-02 12:43:07 -0500

    i think it works

--- v2/app.py
+++ v2/app.py
@@ -9,36 +9,38 @@ app = Flask(__name__)
 def findRecipes(info):
        recipes = getRecipes(info["search"])
        recipeIDs = getRecipeIDs(recipes)
-
+       
        diff = {}
-       for rID in recipeIDs:
-              ingreds = getIngreds(rID)
-              rDiff = calcDiff(ingreds, info)
-              print rDiff
-              '''
+       for rID in recipeIDs[:10]:
+              print rID
               if len(diff) < 3:
-                     diff[rDiff] = rID
-              elif rDiff < max(diff):
-                     del diff[max(diff)]
-                     diff[rDiff] = rID
-       return diff
-       chosen = []
-       chosen1 = recipeIDs[differences.index(min(differences))]
-       differences.pop(differences.index(min(differences)))
-       chosen2 = recipeIDs[differences.index(min(differences))]
-       differences.pop(differences.index(min(differences)))
-       chosen3 = recipeIDs[differences.index(min(differences))]
-       differences.pop(differences.index(min(differences)))
-
-       return chosen
-       '''
+                     ingreds = getIngreds(rID)
+                     rDiff = calcDiff(ingreds, info)
+                     diff[rID] = rDiff
+              elif len(diff) == 3 & getSt(info):
+                     print diff
+                     return diff.keys()
+              elif rDiff < max(diff.values()):
+                     for key, value in diff.items():
+                            if value == max(diff.values()):
+                                   del diff[key]
+                                   break;
+                     ingreds = getIngreds(rID)
+                     rDiff = calcDiff(ingreds, info)
+                     diff[rID] = rDiff
+       return diff.keys()
        
 @app.route('/', methods=['GET','POST'])
 def root():
        if request.method == 'POST':
               info = getInfo()
-              findRecipes(info)
-              return render_template('temp.html', search = info['search'])
+              i = findRecipes(info)
+              ts = []
+              for r in i:
+                     ts += getTS(r)
+              while len(ts) < 6:
+                     ts += ["",""]
+              return render_template('recipes.html', recipe_one = ts[0], recipe1url = ts[1], recipe_two = ts[2], recipe2url = ts[3], recipe_three = ts[4], recipe3url = ts[5])
        else:
               return render_template('index.html')
 
@@ -52,6 +54,9 @@ def getInfo():
        if request.form['fat'] != "":
               info['fat'] = float(request.form['fat'])    
        return info
+
+def getSt(info):
+       return info["protein"] == -1 & info["carb"] == -1 & info["fat"] == -1
 
 if __name__ == '__main__':
 	app.run(debug = True)
--- v2/utils/recipe_func.py
+++ v2/utils/recipe_func.py
@@ -34,11 +34,9 @@ def getRecipeInfo(rID):
 def getIngreds(rID):
         info = getRecipeInfo(rID)
         ingreds = info['ingredients']
-        print ingreds
         d = {}
         for i in ingreds:
                 d.update(cleanup(i))
-        print d
         return d
 
 def cleanup(i):
@@ -128,43 +126,9 @@ def rmxtraWS(i):
         i = " ".join(i.split())
         return i
 
-'''
-for x in range(10):
-        getIngreds(35382 + x)
+def getTS(rID):
+        link = "http://food2fork.com/api/get?key=%s&rId=%s" % (getKey(), rID)
+        r = requests.get(link)
+        d = r.json()
+	return [d['title'], d['source_url']]
 
-def recipe_title(json):
-	return json['title']
-
-def recipe_source_url(json):
-	return json['source_url']
-
-
-a = [
-        u'2 cups (250 grams) all- purpose flour',
-        u'1/2 teaspoon table salt',
-        u'13 tablespoons (185 grams or 1 stick plus 5 tablespoons) cold unsalted butter, diced',
-        u'6 tablespoons (90 grams) sour cream or whole Greek yogurt (i.e., a strained',
-        u'yogurt)',
-        u'1 tablespoon (15 ml) white wine vinegar',
-        u'1/4 cup (60 ml) ice water',
-        u'1 egg, beaten with 1 tablespoon water, for egg wash',
-        u'2 tablespoons (30 ml) olive oil',
-        u'4 ounces (115 grams or 3/4 to 1 cup) 1/4-inch-diced pancetta',
-        u'1 large or 2 small onions, finely chopped',
-        u'1 large carrot, finely chopped',
-        u'1 large stalk celery, finely chopped',
-        u'Pinch of red pepper flakes',
-        u'Salt and freshly ground black pepper',
-        u'2 garlic cloves, minced',
-        u'Thinly sliced Swiss chard leaves from an 8- to 10-ounce (225- to 285-gram)',
-        u'bundle (4 cups); if leaves are very wide, you can halve them lengthwise',
-        u'3 1/2 tablespoons (50 grams) butter',
-        u'3 1/2 tablespoons (25 grams) all- purpose flour',
-        u'3 1/2 cups (765 ml) sodium- free or low- sodium chicken or vegetable broth',
-        u'2 cups white beans, cooked and drained, or from one and a third 15.5- ounce',
-        u'(440-gram) cans'
-]
-
-for x in a:
-        print cleanup(x)
-'''
